fb25f2f915c9e23b3341a1df71979f69887e37c0
Makhuta <matykubla@gmail.com>
2022-07-06 19:40:53 +0200

    Update embed.js

--- src/embed.js
+++ src/embed.js
@@ -3,7 +3,7 @@ const { MessageBuilder } = require("discord-webhook-node");
 module.exports = function (vote) {
   let timestamp = Math.round(Date.now() / 1000);
   let nextVoteTimestamp = timestamp + 43200;
-  console.info(vote)
+  console.info(vote);
   return new MessageBuilder()
     .setTitle(`Voted`)
     .setURL(`https://top.gg/bot/${process.env.TOPGGID}`)
@@ -13,7 +13,7 @@ module.exports = function (vote) {
     .addField("Voted at:", `<t:${timestamp}:f>`, true)
     .addField("Can vote again at:", `<t:${nextVoteTimestamp}:R>`, true)
     .addField("Total Votes:", `TBD`, true)
-    .addField("Is weekend:", vote["isWeekend"].toString().replace("true", "✅").replace("false", "❌"), true)
+    .addField("Is weekend:", vote.isWeekend.replace("true", "✅").replace("false", "❌"), true)
 
     .setColor("RANDOM")
     .setThumbnail("https://cdn.discordapp.com/embed/avatars/0.png")
