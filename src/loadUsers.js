3803be4f1e284535e11a81907fba7b9539fb60f7
Makhuta <matykubla@gmail.com>
2022-07-06 23:16:10 +0200

    Update loadUsers.js

--- src/loadUsers.js
+++ src/loadUsers.js
@@ -5,8 +5,8 @@ async function loadUsers() {
   let sql = "SELECT * FROM userstats";
   let rows = await DBExecuter({ sql });
   for (row of rows) {
-    Userstats[parseInt(row.id)] = {
-      id: parseInt(row.id),
+    Userstats[row.id] = {
+      id: row.id,
       LastVoteTimestamp: parseInt(row.lastvotetimestamp),
       TotalVotes: parseInt(row.totalvotes),
       ComboVotes: parseInt(row.combovotes),
