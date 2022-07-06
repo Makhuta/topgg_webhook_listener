beddad6b93825077d9713290ad2d0f87c60e22a7
Makhuta <matykubla@gmail.com>
2022-07-06 17:48:03 +0200

    Update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const { Webhook } = require(`@top-gg/sdk`)
+const express = require("express");
+const { Webhook } = require(`@top-gg/sdk`);
+const { WebhookClient } = require("discord.js");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
-const wh = new Webhook(process.env.WEBHOOKAUTH)
+const app = express();
+const wh = new Webhook(process.env.WEBHOOKAUTH);
+const discordWH = new WebhookClient(process.env.DISCORDWEBHOOKID, process.env.DISCORDWEBHOOKTOKEN);
 
-app.post('/dblwebhook', wh.listener(vote => {
-  // vote is your vote object e.g
-  console.log(vote.user) // => 321714991050784770
-}))
+app.post(
+  "/dblwebhook",
+  wh.listener((vote) => {
+    webhook.send("Hello world.").catch(console.error);
+    console.log(vote.user); // => 321714991050784770
+  })
+);
 
-app.listen(port)
+app.listen(port);
