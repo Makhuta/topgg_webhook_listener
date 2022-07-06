9779bd076ad48b7e29036b7cb212acafbe065747
Makhuta <matykubla@gmail.com>
2022-07-06 17:24:43 +0200

    Update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const { Webhook } = require(`@top-gg/sdk`)
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.json())
 
-const app = express()
-const wh = new Webhook('webhookauth123')
+const port = process.env.PORT || 3000
 
-app.post('/dblwebhook', wh.listener(vote => {
-  // vote is your vote object e.g
-  console.log(vote.user) // => 321714991050784770
-}))
+app.post("/dialogflow-fulfillment", (request, response) => {
+    console.info("test")
+})
 
-app.listen(80)
+app.listen(port, () => {
+    console.info(`Listening on port ${port}`)
+})
