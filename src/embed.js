9fa681b1f3dc44a33c281161a42720a60f5b9bba
Makhuta <matykubla@gmail.com>
2022-07-06 18:14:48 +0200

    Update embed.js

--- src/embed.js
+++ src/embed.js
@@ -2,14 +2,13 @@ const { MessageBuilder } = require("discord-webhook-node");
 
 module.exports = function (vote) {
   return new MessageBuilder()
-    .setTitle("My title here")
-    .setAuthor("Author here", "https://cdn.discordapp.com/embed/avatars/0.png", "https://www.google.com")
+    .setTitle(`[Voted](https://top.gg/bot/${process.env.TOPGGID})`)
+    .setDescription(`<@${vote.user}> Voted for <@${vote.bot}>!`)
     .setURL("https://www.google.com")
     .addField("First field", "this is inline", true)
     .addField("Second field", "this is not inline")
     .setColor("#00b0f4")
     .setThumbnail("https://cdn.discordapp.com/embed/avatars/0.png")
-    .setDescription("Oh look a description :)")
     .setImage("https://cdn.discordapp.com/embed/avatars/0.png")
     .setFooter("Hey its a footer", "https://cdn.discordapp.com/embed/avatars/0.png")
     .setTimestamp();
