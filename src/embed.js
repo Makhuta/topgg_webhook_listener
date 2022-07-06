d65b4ad1180c040a5ac50846272a06fbc242f67a
Makhuta <matykubla@gmail.com>
2022-07-06 19:08:13 +0200

    Update embed.js

--- src/embed.js
+++ src/embed.js
@@ -1,18 +1,17 @@
 const { MessageBuilder } = require("discord-webhook-node");
 
 module.exports = function (vote) {
-  let timestamp = Math.round(Date.now() / 1000);
-  let nextVoteTimestamp = timestamp + 43200;
-  console.info({ timestamp, nextVoteTimestamp });
+  let timestamp = Date.now();
+  let nextVoteTimestamp = timestamp + 43200000;
   return new MessageBuilder()
     .setTitle(`Voted`)
     .setURL(`https://top.gg/bot/${process.env.TOPGGID}`)
     .setDescription(`<@${vote.user}> Voted for <@${vote.bot}>!`)
-    .addField(
+    .addFields(
       { name: "User:", value: `<@${vote.user}>`, inline: true },
       { name: "Bot:", value: `<@${vote.bot}>`, inline: true },
-      //{ name: "Voted at:", value: `test<t:${timestamp}:f>`, inline: true },
-      //{ name: "Can vote again at:", value: `test<t:${nextVoteTimestamp}:R>`, inline: true }
+      { name: "Voted at:", value: `test<t:${timestamp}:f>`, inline: true },
+      { name: "Can vote again at:", value: `test<t:${nextVoteTimestamp}:R>`, inline: true }
     )
 
     .setColor("RANDOM")
