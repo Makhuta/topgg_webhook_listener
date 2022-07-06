ae70230558c80c19feca8434cc771e75cf6afc6c
Makhuta <matykubla@gmail.com>
2022-07-06 18:34:23 +0200

    Update embed.js

--- src/embed.js
+++ src/embed.js
@@ -1,13 +1,20 @@
 const { MessageBuilder } = require("discord-webhook-node");
 
 module.exports = function (vote) {
+  let timestamp = new Date.now();
+  let nextVoteTimestamp = timestamp + 43200000;
   return new MessageBuilder()
     .setTitle(`Voted`)
     .setURL(`https://top.gg/bot/${process.env.TOPGGID}`)
     .setDescription(`<@${vote.user}> Voted for <@${vote.bot}>!`)
-    .addField("First field", "this is inline", true)
-    .addField("Second field", "this is not inline")
-    .setColor("#00b0f4")
+    .addField(
+      { name: "User:", value: `<@${vote.user}>`, inline: true },
+      { name: "Bot:", value: `<@${vote.bot}>`, inline: true },
+      { name: "Voted at:", value: `<t:${timestamp}:f>`, inline: true },
+      { name: "Can vote again at:", value: `<t:${nextVoteTimestamp}:R>`, inline: true }
+    )
+
+    .setColor("RANDOM")
     .setThumbnail("https://cdn.discordapp.com/embed/avatars/0.png")
     .setImage("https://cdn.discordapp.com/embed/avatars/0.png")
     .setFooter("Hey its a footer", "https://cdn.discordapp.com/embed/avatars/0.png")
